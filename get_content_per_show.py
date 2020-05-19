c46fde503e66c3a9b47d98619433b35b7779d37d
livia_test <livia_test@liviatestsimac.home>
2020-05-19 09:08:54 -0400

    even more data

--- get_content_per_show.py
+++ get_content_per_show.py
@@ -56,7 +56,7 @@ for key, value in show_urls.items():
         # in a given key pair + how many directories in the "year" dir
         # ...
         # skip if not in valid range:
-        if i<2740:
+        if i<2840:
             i+=1
             continue
 
