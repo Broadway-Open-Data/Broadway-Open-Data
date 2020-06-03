43b805916e9566d2b8c9a09d9dc73b1dc4c750f5
Ephraim Klestzick <47016202+effieklestz@users.noreply.github.com>
2020-06-02 22:14:00 -0400

    this is strange

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def do_something():
     """A placeholder function"""
-    print("successful!")
+    print("word!")
 
 
 # ==============================================================================
