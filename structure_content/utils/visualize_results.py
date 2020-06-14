ab60982334b62aacb3efb16e80f3989f0e4528ed
ybressler <yaakovbressler@gmail.com>
2020-06-14 00:29:26 -0400

    created code which will structure all show info (easy one).

--- structure_content/utils/visualize_results.py
+++ structure_content/utils/visualize_results.py
@@ -22,13 +22,14 @@ pd.options.display.max_columns = 150
 pd.options.display.width = 1500
 
 # Load the current data
-curr_data_path = Path(os.path.join("data","all_show.json"))
+curr_data_path = Path(os.path.join("data","all_shows.json"))
 
 with open(curr_data_path,"r") as f:
     all_data = json.load(f)
 
 
 df = pd.json_normalize(all_data)
+
 drop_cols = df.columns[df.columns.str.endswith('_URL')]
 df.drop(columns=drop_cols, inplace=True)
 
