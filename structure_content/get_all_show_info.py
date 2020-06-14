09ceb3291ef17898817b6d22627f241342bfc04c
ybressler <yaakovbressler@gmail.com>
2020-06-14 02:08:29 -0400

    finished breaking down the data for shows & people.

--- structure_content/get_all_show_info.py
+++ structure_content/get_all_show_info.py
@@ -8,10 +8,8 @@ import re
 import sys
 import json
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from pathlib import Path
-from IPython.display import display
 sys.path.append('..')
 
 # Set pandas display options
@@ -24,51 +22,32 @@ pd.options.display.width = 1500
 # Load the current data
 curr_data_path = Path(os.path.join("data","all_shows.json"))
 
+if not os.path.isfile(curr_data_path):
+    raise AssertionError(f"file doesn't exist for '{curr_data_path}'")
+    sys.exit()
+
 with open(curr_data_path,"r") as f:
     all_data = json.load(f)
-
-# These are the unique keys for `show_info`
-show_info_keys = [
-    "title",
-    "Other titles",
-    "table_label",
-    "Opening Info",
-    "Previews",
-    "Opening",
-    "Closing",
-    "# Performances",
-    "Theatres",
-    "Production Type",
-    "Run Type",
-    "Market",
-    "Running Time",
-    "Intermissions",
-    "Official Website",
-    "Version",
-    "Show type",
-    "Run Type",
-    ]
 
 all_show_info = []
 
 for show_record in all_data:
-    show_info_record = show_record["show_info"]
+    record = show_record["show_info"]
 
     # Delete this if it exists...
-    if "table_label" in show_info_record.keys():
-        del show_info_record["table_label"]
+    if "table_label" in record.keys():
+        del record["table_label"]
 
     # Add the following values:
     for x in ["show_id","year"]:
-        show_info_record[x] = show_record.get(x)
+        record[x] = record.get(x)
 
     # Save
-    all_show_info.append(show_info_record)
+    all_show_info.append(record)
 
+# -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 # Save here when finished
+print(f"saving data for {len(all_show_info)} records")
 save_data_path = Path(os.path.join("data","all_show_info.json"))
 with open(save_data_path,"w") as f:
     json.dump(all_show_info,f)
-
-
-# print(show_record["show_info"].keys())
