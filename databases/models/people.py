207b13083c83c392f525b4a130dec726ee6944a1
ybressler <yaakovbressler@gmail.com>
2020-10-13 22:18:21 -0400

    splitting gender identity to id and name #15

--- databases/models/people.py
+++ databases/models/people.py
@@ -125,7 +125,7 @@ class Person(db.Model, models.dbTable):
     # --------------------------------------------------------------------------
     # Here's where I need help with...
     # 1:1 relationship – Have to figure this out later...
-    gender_identity = db.Column(db.String, nullable=False, db.ForeignKey('gender_identity.id'))
+    gender_identity_id = db.Column(db.Integer, db.ForeignKey('gender_identity.id'))
 
     # --------------------------------------------------------------------------
 
