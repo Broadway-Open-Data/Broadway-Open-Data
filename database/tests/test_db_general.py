71f91cbe01a61b4d3e316f1b8968ef511b1a0cff
ybressler <yaakovbressler@gmail.com>
2021-01-10 23:43:18 -0500

    Created a testing module to assert data integrity in models. #21

---
+++ database/tests/test_db_general.py
@@ -0,0 +1,44 @@
+import sys
+import os
+
+
+if os.environ.get('PROJECT_PATH'):
+    sys.path.append(os.environ.get('PROJECT_PATH'))
+else:
+    sys.path.append('database')
+
+
+# from database import models
+from database.models.shows import Show
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.dialects.mysql import TINYINT, YEAR
+
+
+
+
+show_fields = {x.name:x.type for x in Show.__table__.c}
+expected_show_fields = {'id': Integer(),
+ 'date_instantiated': DateTime(),
+ 'title': String(length=200),
+ 'opening_date': DateTime(),
+ 'closing_date': DateTime(),
+ 'previews_date': DateTime(),
+ 'year': YEAR(),
+ 'theatre_id': Integer(),
+ 'scraped_theatre_name': String(length=60),
+ 'production_type': String(length=20),
+ 'show_type': String(length=20),
+ 'show_type_simple': String(length=20),
+ 'intermissions': TINYINT(),
+ 'n_performances': Integer(),
+ 'run_time': Integer(),
+ 'show_never_opened': Boolean(),
+ 'revival': Boolean(),
+ 'pre_broadway': Boolean(),
+ 'limited_run': Boolean(),
+ 'repertory': Boolean(),
+ 'other_titles': String(length=300),
+ 'official_website': String(length=100)}
+
+for key, value in show_fields.items():
+    assert type(expected_show_fields[key]) == type(value)
